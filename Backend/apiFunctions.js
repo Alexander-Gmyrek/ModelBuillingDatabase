c5e9344a81229cc35bf46aa21d549b3a207b86c0
Alexender-Gmyrek <agdaboss12@gmail.com>
2025-04-22 18:13:20 -0700

    Making the installer work.

--- Backend/apiFunctions.js
+++ Backend/apiFunctions.js
@@ -3,7 +3,9 @@
 
 //export default config;
 
-const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
+const BASE_URL = (window.BASE_API_URL || "http://localhost:3000") + "/api";
+
+
 
 
 // Helper function to make API requests
